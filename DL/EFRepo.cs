aece6f7cb992470ee6bdf40b7216c2f04c32925c
LornaGrajek <lorna.grajek@gmail.com>
2022-01-28 13:11:01 -0800

    methods for category: get all, add, delte, search

--- DL/EFRepo.cs
+++ DL/EFRepo.cs
@@ -10,11 +10,26 @@ public class EFRepo : IRepo
     {
         _context = context;
     }
+    public List<Category> GetAllCategories()
+    {
+        return _context.Categories.Select(r => r).ToList();
+    }
     public Category AddCategory(Category catToAdd)
     {
         _context.Add(catToAdd);
         _context.SaveChanges();
 
         return catToAdd;
+    }
+    public void DeleteCategory(Category catToDelete)
+    {
+        _context.Remove(catToDelete);
+        _context.SaveChanges();
+        _context.ChangeTracker.Clear();
+    }
+
+    public List<Category> SearchCategories(string searchTerm)
+    {
+        return _context.Categories.Where(x => x.CategoryName.ToLower().Contains(searchTerm.ToLower())).ToList();
     }
 }
