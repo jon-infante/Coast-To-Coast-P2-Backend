f097b112c4508cb22eb5afab097f39d6318139e3
LornaGrajek <lorna.grajek@gmail.com>
2022-01-28 13:33:25 -0800

    updated IRepo

--- DL/IRepo.cs
+++ DL/IRepo.cs
@@ -2,5 +2,9 @@ namespace DL;
 
 public interface IRepo
 {
-    
+    List<Category> GetAllCategories();
+    Category GetCategoryById(int ID);
+    void AddCategory(Category catToAdd);
+    void DeleteCategory(Category catToDelete);
+    List<Category> SearchCategories(string searchTerm);
 }
