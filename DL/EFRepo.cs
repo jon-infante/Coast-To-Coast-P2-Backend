efdf6a021e40bfe2b0f2d7efe3d64802de775117
Khanh Nguyen <knguye15.ccp@gmail.com>
2022-01-31 14:55:27 -0500

    initial branch cimmit

--- DL/EFRepo.cs
+++ DL/EFRepo.cs
@@ -1,4 +1,4 @@
-using Model = Models;
+using Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DL;
@@ -11,5 +11,52 @@ public class EFRepo : IRepo
         _context = context;
     }
 
-    
+    public List<Like> GetLikeByUserID(Like lk)
+    {
+        List<Like> likes = new List<Like> ();
+        likes = _context.Likes.where(l => l.UserID = lk.UserID).ToList();
+        return likes;
+    }
+
+    public List<Like> GetLikeByDrawingID(Like lk)
+    {
+        List<Like> likes = new List<Like> ();
+        likes = _context.Likes.where(l => l.DrawingID = lk.DrawingID).ToList();
+        return likes;
+    }
+
+    public List<Like> GetLikeByCommentID(Like lk)
+    {
+        List<Like> likes = new List<Like> ();
+        likes = _context.Likes.where(l => l.CommentID = lk.CommentID).ToList();
+        return likes;
+    }
+
+    public Like NewLike (Like lk)
+    {
+        Like like = new Like ();
+        like = _context.Likes.Find.where(like => Likes.UserID = lk.UserID && Likes.DrawingID = lk.DrawingID && like.CommnetID = lk.CommentID);
+        if (like == null)
+        {
+            like = _context.Likes.Add(lk);
+        }
+        return like;
+    }
+
+    public void RemoveLike (Like lk)
+    {
+        Like like = new Like ();
+        like = _context.Likes.Find.where(like => Likes.UserID = lk.UserID && Likes.DrawingID = lk.DrawingID && like.CommnetID = lk.CommentID);
+        if (like != null)
+        {
+            like = _context.Likes.Remove(lk);
+        }
+    }
+
+    public List<Comment> GetCommentByDrawingID(Drawing draw)
+    {
+        List<Comment> comments = new List<Comment>();
+        comments = _context.Comments.Find.where(c => c.DrawingID = draw.ID);
+        return comments;
+    }
 }
